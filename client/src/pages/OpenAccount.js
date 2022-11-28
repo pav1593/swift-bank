2a055de058485b74c36924a70d6394d2eedbf5c9
EllisJLC <jia.liang.chen@ryerson.ca>
2022-11-28 02:38:42 -0500

    added global state, added open account page

--- client/src/pages/OpenAccount.js
+++ client/src/pages/OpenAccount.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useUserContext } from '../utils/GlobalState';
+import { CREATE_USER } from '../utils/actions';
 
 export default function OpenAccount() {
+  const [state, dispatch] = useUserContext();
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    dispatch({
+      type: CREATE_USER,
+      firstName,
+      lastName,
+      email,
+      password
+    })
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  }
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
   return (
     <Box
       component="form"
@@ -18,16 +52,19 @@ export default function OpenAccount() {
             required
             id="outlined-required"
             label="Email"
+            onChange={handleEmailChange}
           />
           <TextField
             required
             id="outlined-required"
             label="First Name"
+            onChange={handleFirstNameChange}
           />
           <TextField
             required
             id="outlined-required"
             label="Last Name"
+            onChange={handleLastNameChange}
           />
           <br/>
           <TextField
@@ -36,10 +73,11 @@ export default function OpenAccount() {
             label="Password"
             type="password"
             autoComplete="current-password"
+            onChange={handlePasswordChange}
           />
         </div>
         <br/>
-        <Button variant="contained">Sign up</Button>
+        <Button variant="contained" onSubmit={handleSubmit}>Sign up</Button>
     </Box>
   )
 }
