0a9acd13cbfea04f7a4fef8a92ae261ef1674754
EllisJLC <jia.liang.chen@ryerson.ca>
2022-11-28 23:33:10 -0500

    thiingy

--- client/src/components/GlobalState.js
+++ client/src/components/GlobalState.js
@@ -5,7 +5,12 @@ const UserContext = createContext(); // create user context
 const {Provider} = UserContext; // isolates provider from context, provider feeds to state change observers, consumers
 
 const UserProvider = ({ value = [], ...props }) => { 
-  const [state, dispatch] = useUserReducer({});
+  const [state, dispatch] = useUserReducer({
+    isLoggedIn: false,
+    isAdmin: false,
+    userName: 'admin',
+    clientId: ""
+  });
 
   return <Provider value={[state, dispatch]} {...props}/> // pass the state and dispatch from the reducer to the provider
 };
