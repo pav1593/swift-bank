42a8eab8ee674b58bd08328736683862b35bc405
Nick Pavlovic <nickpavlovic@icloud.com>
2022-12-05 12:57:32 -0500

    made static footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,15 +3,21 @@ import React from "react";
 const styles = {
   footer: {
     background: "#2c3437",
+    color: "gray", 
+    position: "fixed", 
+    bottom: 0,
+    width: '100%',
+    height: '15%'
   }
 }
 
 export default function Footer() {
   return (
     <footer style={styles.footer}>
-      <p>Created in November 2022</p>
       <br/>
-      <p>Contact us <a href="#">here</a>!</p>
+      <center>Copyright @ 2022 Swift Bank...</center>
     </footer>
   );
+
+  
 }
